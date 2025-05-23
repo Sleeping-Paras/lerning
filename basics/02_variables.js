208392a007b75817ef3a00e5b0a913764775f46a
Sleeping-Paras <pk035466@gmail.com>
2025-05-23 07:25:15 +0000

    done

---
+++ basics/02_variables.js
@@ -0,0 +1,8 @@
+"use strict" ;// means we are using new version
+ // number > 2 to power 52 
+ // big int 
+ // string 
+ // bool
+ // null > value represent nothing (but its an onject )
+ // undefind > somthing which is not defind 
+ // symbol > unique data typr hai 
---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const account_id = 123 // not allowed to change 
+let me =  "paras"
+var passwerd = "1234"// no scope {} walla forget it 
+jsi = "kung-fu-panda"
+
+console.log(jsi);
+console.table([account_id , me ])
