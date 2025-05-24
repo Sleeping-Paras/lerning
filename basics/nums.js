4603a2f37ca2d976413bc28b609018ce1d18f453
Sleeping-Paras <pk035466@gmail.com>
2025-05-24 20:24:08 +0000

    done

--- basics/nums.js
+++ basics/nums.js
@@ -1,5 +1,29 @@
-const score =  400;
+// const score =  400;
 
-const num = new Number(400);
-console.log(num);
-console.log(score);
+// const num = new Number(400);// confermed that it is num
+// console.log(num);
+// console.log(score);
+
+// console.log(num.toString().length) // converts num to string 
+//  const x = 123.777
+
+//  console.log(x.toPrecision(3)) // will return 3 digit of x with approximation( it returns 124) 
+
+//  const y = 123.22222
+
+//  console.log(y.toPrecision(3)) // returns 123 in this case
+
+ // +++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(23.999999));
+// console.log(Math.floor(23.4444));
+// console.log(Math.min(3,4,56,66,66,66,4));
+console.log(Math.random()*10 + 1);
+const mx =  20;
+const mn = 10;
+
+console.log(Math.floor(Math.random()*(mx - mn + 1 )) + mn);// its imp// whenever we want to defin some mxa ans some min we do this
+
+
