861dc28d5bf7ac7bfc14d5cc356f451fe707f6a5
Sleeping-Paras <pk035466@gmail.com>
2025-05-31 13:40:52 +0000

    done

---
+++ ds/object.js
@@ -0,0 +1,14 @@
+// singeleton joo contructor se nahi banta 
+
+// object letrels
+
+const user = {
+
+    name: "paras",
+    age : 22,
+    location : "jaipur",
+    email : "paras@google.com",
+    islogin : false
+} 
+
+console.log(user["email"]);
