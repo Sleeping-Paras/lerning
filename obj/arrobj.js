a4fdbbb9b22b18e0fc61db8d5ab6aebbcbd5879a
Sleeping-Paras <pk035466@gmail.com>
2025-07-01 19:27:09 +0000

    paras

---
+++ obj/arrobj.js
@@ -0,0 +1,7 @@
+// obj in arr
+
+let arr = [
+    {user1 : "paras"},
+    {user1 : "jai"},
+    {user3 : "monika"}
+]
--- obj/obj.js
+++ obj/obj.js
@@ -1,1 +1,11 @@
 
+let obj = {
+    "name" : "paras",
+    "age" : 23,
+    "last name" : "londa",
+    fun : function(a){
+          console.log(a);
+    }
+}
+
+obj["fun"](34);  
